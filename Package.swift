478c19428bcfdd1c9927e9be48f01467535054e0
skipp <chanchanakoedtho@gmail.com>
2022-05-13 11:44:45 +0700

    Fixed gitignore

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "TransitionableTab",
+    platforms: [
+           .iOS(.v8)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
